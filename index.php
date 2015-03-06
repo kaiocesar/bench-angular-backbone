317e2efa45ace51791c373c32e5fc932cbcd7ed6
Desenvolvimento IronSystems <desenvolvimento@ironsystems.com.br>
2015-03-06 17:57:38 -0300

    chg[] initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" ng-app id="ng-app">
+<head>
+	<meta chartset="utf-8" />
+	<title>Benchmarking angular.js x backbone.js</title>
+	<script type="text/javascript" src="assets/js/angular.min.js"></script>
+</head>
+<body>
+
+
+<div class="ng-app" ng-controller="PostsCtr">
+	
+</div>
+http://www.9lessons.info/2013/08/angularjs-tutorial-restful.html
+
+
+
+</body>
+</html>
